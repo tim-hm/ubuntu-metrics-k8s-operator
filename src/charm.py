0ff8b2898a55c641d3db52f8dbdfe663b2ba4ca8
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-03-01 15:30:05 +0000

    feat: Improve charm structure and expose version
    * Rename WorkloadConfigManager to WorkloadManager because sometimes it
    does more than simply manage the config.
    * Use class properties and 'private' values more.
    * Use service's `/about` endpoint to expose the workload's version.

--- src/charm.py
+++ src/charm.py
@@ -6,7 +6,7 @@ import logging
 from typing import TYPE_CHECKING
 
 import ops
-from workload_config_manager import WorkloadConfigManager
+from workload_manager import WorkloadManager
 
 # if development bring in types via dev paths
 if TYPE_CHECKING:
@@ -19,31 +19,35 @@ logger = logging.getLogger(__name__)
 
 
 class UbuntuReportd(ops.CharmBase):
+    _container: ops.Container
+    _workload: WorkloadManager
 
     def __init__(self, *args):
         super().__init__(*args)
-        self._container = self.unit.get_container(WorkloadConfigManager.name)
-        self._app_manager = WorkloadConfigManager()
+        self._container = self.unit.get_container(WorkloadManager.name)
+        self._workload = WorkloadManager(self.container)
 
-        self.framework.observe(self.on[WorkloadConfigManager.name].pebble_ready, self._on_pebble_ready)
+        self.framework.observe(self.on[WorkloadManager.name].pebble_ready, self._on_pebble_ready)
 
     def _on_pebble_ready(self, _: ops.PebbleReadyEvent):
         self.unit.status = ops.MaintenanceStatus("Configuring container")
 
-        if not self._container.can_connect():
+        if not self.container.can_connect():
             self.unit.status = ops.WaitingStatus("Waiting for Pebble to be available")
             return
 
-        if not self._app_manager.ready():
+        if not self.workload.ready():
             self.unit.status = ops.WaitingStatus("Waiting for AppManager to be ready")
             return
 
         try:
-            self._container.add_layer(
-                WorkloadConfigManager.name, self._app_manager.pebble_layer(), combine=True
+            self.container.add_layer(
+                WorkloadManager.name, self.workload.pebble_layer, combine=True
             )
-            self._container.replan()
-            self.unit.open_port(protocol="tcp", port=self._app_manager.port)
+            self.container.replan()
+            self.unit.open_port(protocol="tcp", port=self.workload.port)
+
+            self.unit.set_workload_version(self.workload.version())
             self.unit.status = ops.ActiveStatus()
         except ops.pebble.ConnectionError as e:
             logger.error(f"Failed to connect to Pebble: {e}")
@@ -51,6 +55,14 @@ class UbuntuReportd(ops.CharmBase):
         except Exception as e:
             logger.error(f"Pebble replan failed: {e}")
             self.unit.status = ops.BlockedStatus("Failed to configure container")
+
+    @property
+    def container(self) -> ops.Container:
+        return self._container
+
+    @property
+    def workload(self) -> WorkloadManager:
+        return self._workload
 
 
 if __name__ == "__main__":  # pragma: nocover
