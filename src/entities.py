92ed807cdf77e7af4d8e09509ac529b6a63b0909
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-03-04 16:43:11 +0000

    feat: Add WorkloadBuilder to simplify charm initialisation
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

---
+++ src/entities.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+from utils import get_or_fail
+
+
+@dataclass
+class About:
+    version: str
+
+
+@dataclass
+class Workload:
+    name: str
+    port: int
+    db_name: str
+    db_relation_name: str
+    db_host: str
+    db_port: int
+    db_username: str
+    db_password: str
+
+
+class WorkloadState(Enum):
+    Ready = 1
+    ContainerNotReady = 2
+    DbNotReady = 3
+
+
+class WorkloadBuilder:
+    def __init__(self) -> None:
+        self.name: Optional[str] = None
+        self.port: Optional[int] = None
+        self.db_name: Optional[str] = None
+        self.db_relation_name: Optional[str] = None
+        self.db_host: Optional[str] = None
+        self.db_port: Optional[int] = None
+        self.db_username: Optional[str] = None
+        self.db_password: Optional[str] = None
+
+    def set_name(self, value: str) -> "WorkloadBuilder":
+        self.name = value
+        return self
+
+    def set_port(self, value: int) -> "WorkloadBuilder":
+        self.port = value
+        return self
+
+    def set_db_name(self, value: str) -> "WorkloadBuilder":
+        self.db_name = value
+        return self
+
+    def set_db_relation_name(self, value: str) -> "WorkloadBuilder":
+        self.db_relation_name = value
+        return self
+
+    def set_db_host(self, value: str) -> "WorkloadBuilder":
+        self.db_host = value
+        return self
+
+    def set_db_port(self, value: int) -> "WorkloadBuilder":
+        self.db_port = value
+        return self
+
+    def set_db_username(self, value: str) -> "WorkloadBuilder":
+        self.db_username = value
+        return self
+
+    def set_db_password(self, value: str) -> "WorkloadBuilder":
+        self.db_password = value
+        return self
+
+    def check_is_ready(self) -> bool:
+        return all(
+            value is not None
+            for value in [
+                self.name,
+                self.port,
+                self.db_name,
+                self.db_relation_name,
+                self.db_host,
+                self.db_port,
+                self.db_username,
+                self.db_password,
+            ]
+        )
+
+    def build(self) -> Workload:
+        return Workload(
+            name=get_or_fail(self.name, "name"),
+            port=get_or_fail(self.port, "port"),
+            db_name=get_or_fail(self.db_name, "db_name"),
+            db_relation_name=get_or_fail(self.db_relation_name, "db_relation_name"),
+            db_host=get_or_fail(self.db_host, "db_host"),
+            db_port=get_or_fail(self.db_port, "db_port"),
+            db_username=get_or_fail(self.db_username, "db_username"),
+            db_password=get_or_fail(self.db_password, "db_password"),
+        )
