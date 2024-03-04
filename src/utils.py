66e856eb3627ac3e82300bd26e5a316e5622a98e
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-03-04 16:42:40 +0000

    feat: Add utils for stringify and get_or_fail(optional[T])
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from typing import Any, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+def get_or_fail(value: Optional[T], name: str = "unknown") -> T:
+    if value is None:
+        raise ValueError(f"{name} was None")
+    return value
+
+
+def stringify(instance: Any) -> str:
+    class_name = instance.__class__.__name__
+    properties_str = ", ".join(f"{attr}={value}" for attr, value in instance.__dict__.items())
+    return f"{class_name}{{{properties_str}}}"
