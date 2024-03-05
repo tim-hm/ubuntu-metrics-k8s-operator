8d08cf70ba1eb7492fa1c926c541990b41069d65
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-03-05 13:58:50 +0000

    feat: Add deploy and refresh helper scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 juju deploy \
     ./ubuntu-reportd_ubuntu-22.04-amd64.charm \
-    --resource image=ghcr.io/tim-hm/ubuntu-report:sha-69ce01e
+    --resource image=ghcr.io/tim-hm/ubuntu-report:dev
--- scripts/pack-refresh.sh
+++ scripts/pack-refresh.sh
@@ -8,4 +8,4 @@ juju refresh \
     ubuntu-reportd \
     --path=./ubuntu-reportd_ubuntu-22.04-amd64.charm \
     --force-units \
-    --resource image=ghcr.io/tim-hm/ubuntu-report:sha-69ce01e
+    --resource image=ghcr.io/tim-hm/ubuntu-report:dev
