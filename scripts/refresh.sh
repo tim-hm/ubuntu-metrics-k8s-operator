788aedbd507a02a1a640495e1f26abf960ba26a7
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-04-12 13:30:44 +0100

    ci: Refine deploy and refresh scripts
    - Update charm name to ubuntu-metrics
    - Use bash script options (ie set -euxo pipefail)
    - Explicilty target desktop model

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -6,7 +6,9 @@ TAG="sha-${1}"
 
 charmcraft pack
 
-juju deploy \
-    ./ubuntu-metrics_ubuntu-22.04-amd64.charm \
+juju refresh \
+    ubuntu-metrics \
     --model=desktop \
+    --path=./ubuntu-metrics_ubuntu-22.04-amd64.charm \
+    --force-units \
     --resource "image=ghcr.io/tim-hm/ubuntu-report:$TAG"
